bfaa9ff6e7a478bab85f05b764c3ab910aeaced0
vinothbabu <vinothbabu@10decoders.in>
2023-08-03 19:14:22 +0530

    HOC,Protected

--- Routers/routers/src/Components/Nomatch.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Nomatch = () => {
-  return (
-    <div> No such page is found </div>
-  )
-}
-export default Nomatch
--- Routers/routers/src/Components/UserDetails.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const UserDetails = () => {
-  return (
-    <div>UserDetails Displayed here</div>
-  )
-}
-export default UserDetails
