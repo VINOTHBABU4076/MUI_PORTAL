c9a506c5321d26da1ee5a946706bf2a0d933199a
vinothbabu <vinothbabu@10decoders.in>
2023-08-11 17:30:06 +0530

    Crud

--- lazy/src/App.js
+++ lazy/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState} from 'react'
+import axios from 'axios'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 function App() {
+
+  const url=`https://jsonplaceholder.typicode.com/photos`
+ 
+  const[images,setImages]=useState([])
+
+  const getImages =() =>{
+    axios.get(url).then(res=>{
+      console.log(res.data)
+      setImages(res.data)
+    })
+  }
+  useEffect (()=>{
+    getImages()
+ },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       hello
+       {images.map(image=>{
+        return(<LazyLoadImage
+          effect='blur' src={image.url} alt='' key={image.id}></LazyLoadImage>)
+       })}
     </div>
   );
 }
