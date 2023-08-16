6d42e5a26db8f62c9ed268b91bc53b7781f305d8
vinothbabu <vinothbabu@10decoders.in>
2023-08-16 19:17:54 +0530

    Vercel

--- lazy/src/App.js
+++ lazy/src/App.js
@@ -1,31 +1,36 @@
 import './App.css';
-import {useEffect,useState} from 'react'
-import axios from 'axios'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
 function App() {
+  const apiUrl = process.env.REACT_APP_API_URL;  
 
-  const url=`https://jsonplaceholder.typicode.com/photos`
- 
-  const[images,setImages]=useState([])
+  const [images, setImages] = useState([]);
 
-  const getImages =() =>{
-    axios.get(url).then(res=>{
-      console.log(res.data)
-      setImages(res.data)
-    })
-  }
-  useEffect (()=>{
-    getImages()
- },[])
+  const getImages = () => {
+    axios.get(apiUrl).then((res) => {
+      console.log(res.data);
+      setImages(res.data);
+    });
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
   return (
     <div className="App">
-       hello
-       {images.map(image=>{
-        return(<LazyLoadImage
-          effect='blur' src={image.url} alt='' key={image.id}></LazyLoadImage>)
-       })}
+      hello
+      {images.map((image) => (
+        <LazyLoadImage
+          effect="blur"
+          src={image.url}
+          alt=""
+          key={image.id}
+        ></LazyLoadImage>
+      ))}
     </div>
   );
 }
